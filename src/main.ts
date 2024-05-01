0f0bfca35e71470309a342502bc6db141388aed0
Shivam Singh Kushwaha <ssk7860345351@gmail.com>
2024-05-01 21:38:28 +0530

    changes in swagger

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document, {
+    customSiteTitle: 'Nest JS Practice',
     customJs: [
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
